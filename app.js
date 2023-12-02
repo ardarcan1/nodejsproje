680657160838f9599f8bd43e5a8014874d5ee6ab
Arda Arcan <ardacan-arcan@hotmail.com>
2023-12-03 01:48:21 +0300

    app eklendi

---
+++ app.js
@@ -0,0 +1,15 @@
+// HTTP modülünü yükle
+const http = require("http");
+
+// HTTP sunucusunu oluştur ve 8000 portunda istekleri dinle
+http.createServer((request, response) => {
+
+    // Yanıt HTTP başlığını HTTP durumu ve İçerik tipi ile ayarla
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // Yanıt gövdesini "Hello World" olarak gönder
+    response.end('Hello World\n');
+}).listen(8000);
+
+// Konsol mesajı yazdırır
+console.log('Server running at http://127.0.0.1:8000/');
